043f832fbc61a88bb71d5ec5a96c01e1013b96ab
Alex Sanchez <alexs@glue.digital>
2025-02-03 13:12:58 +0100

    add(config): use require in webpack virtual index

--- src/index.mjs
+++ src/index.mjs
@@ -30,21 +30,21 @@ class JsconfdPlugin {
   }
 
   addVirtualIndex(compiler, files) {
-    const name = compiler.options.name || 'default';
-    const modulePath = path.resolve(__dirname, `__virtual-conf-${name}.js`);
+    const name = compiler.options.name || 'default'
+    const modulePath = path.resolve(__dirname, '__virtual-conf-' + name + '.js')
 
-    let contents = '';
-    contents += 'const files = await Promise.all([\n';
+    let contents = ''
+    contents += 'const files = [\n'
     for (const file of files) {
-      contents += `import('${file.replace(/\\/g, '\\\\')}'),\n`;
+      contents += `require('${file.replace(/\\/g, '\\\\')}'),\n`
     }
-    contents += ']);\n';
-    contents += `const merge = ${this.opts.merge ? this.opts.merge.toString() : 'Object.assign'};\n`;
-    contents += 'const ret = files.reduce((acc, curr) => merge(acc, curr.default || curr), {});\n';
-    contents += 'export default ret;\n';
+    contents += ']\n'
+    contents += 'const merge = ' + (this.opts.merge ? this.opts.merge.toString() : 'Object.assign') + '\n'
+    contents += 'const ret = files.reduce((acc, curr) => merge(acc, curr), {})\n'
+    contents += 'export default ret\n'
 
-    this.vmp.writeModule(modulePath, contents);
-  }
+    this.vmp.writeModule(modulePath, contents)
+   }
 
   resolve(compiler, resolver, request, resolveContext, callback) {
     if (request.request === '#js.conf.d') {
